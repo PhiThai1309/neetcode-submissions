21756504bbb6f936df06383efea348a46e9a1e74
Thai Manh Phi <71892904+PhiThai1309@users.noreply.github.com>
2026-05-10 00:37:59 +0700

    Add: is-palindrome - submission-4

---
+++ Data Structures & Algorithms/is-palindrome/submission-4.js
@@ -0,0 +1,39 @@
+class Solution {
+    /**
+     * @param {string} s
+     * @return {boolean}
+     */
+    isPalindrome(s) {
+        let left = 0;
+        let right = s.length - 1;
+
+        while (left <= right) {
+            while (left <= right && !this.isAlphaNumeric(s[left])) {
+                left++;
+            }
+
+            while (left <= right && !this.isAlphaNumeric(s[right])) {
+                right--;
+            }
+
+            if (left > right) break;
+
+            if (s[left].toLowerCase() !== s[right].toLowerCase()) {
+                return false;
+            }
+
+            left++;
+            right--;
+        }
+
+        return true;
+    }
+
+    isAlphaNumeric(ch) {
+        return (
+            (ch >= 'a' && ch <= 'z') ||
+            (ch >= 'A' && ch <= 'Z') ||
+            (ch >= '0' && ch <= '9')
+        );
+    }
+}

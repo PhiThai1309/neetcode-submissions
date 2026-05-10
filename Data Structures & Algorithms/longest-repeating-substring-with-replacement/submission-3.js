b7ef8858b73c91583adff2aeb01ea2a0df2e36d6
Thai Manh Phi <71892904+PhiThai1309@users.noreply.github.com>
2026-05-10 23:50:46 +0700

    Add: longest-repeating-substring-with-replacement - submission-3

---
+++ Data Structures & Algorithms/longest-repeating-substring-with-replacement/submission-3.js
@@ -0,0 +1,32 @@
+class Solution {
+    /**
+     * @param {string} s
+     * @param {number} k
+     * @return {number}
+     */
+    characterReplacement(s, k) {
+        let left = 0;
+        let map = new Map();
+        let maxFreq = 0;
+        let longest = 0
+
+        for (let i = 0; i < s.length; i++) {
+            if (!map.has(s[i])) {
+                map.set(s[i], 1);
+            } else {
+                map.set(s[i], map.get(s[i]) + 1);
+            }
+
+                maxFreq = Math.max(maxFreq, map.get(s[i]));
+
+            while (i - left + 1 - maxFreq > k) {
+                map.set(s[left], map.get(s[left]) - 1);
+                left++;
+            }
+
+            longest = Math.max(longest, i - left + 1)
+        }
+
+        return longest;
+    }
+}
